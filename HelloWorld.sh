92102bc6297de47b349188c832db3e79bdbdd706
He Yangle <vooum@qq.com>
2019-11-29 21:39:05 +0800

    seperate sudo commands

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "开始安装"
-apt clean
-apt update
-apt upgrade
-echo y | apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy scipy pandas matplotlib ipython
+sudo apt clean
+sudo apt update
+echo y | sudo apt upgrade
+echo y | sudo apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U # 用清华源升级pip
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+echo y | pip3 install numpy scipy pandas matplotlib ipython jupyter
