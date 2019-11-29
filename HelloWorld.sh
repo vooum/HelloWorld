81f225ffaf4fcc066620ed8a5ebaddcc0d9a63d7
He Yangle <vooum@qq.com>
2019-11-29 20:35:34 +0800

    cancle gcc4s

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -4,5 +4,5 @@ echo "开始安装"
 apt clean
 apt update
 apt upgrade
-apt install gcc{,-4.8} g++{,-4.8} gfortran{,-4.8} python3{,-dev,-pip,-tk} git cmake vim screen
+apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
 pip3 install numpy scipy pandas matplotlib ipython
