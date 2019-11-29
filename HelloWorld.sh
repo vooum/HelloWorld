25428918ca44bfdce91d73a280558a910c07bfbb
He Yangle <vooum@qq.com>
2019-11-29 20:46:25 +0800

    add TsingHua source

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -4,5 +4,5 @@ echo "开始安装"
 apt clean
 apt update
 apt upgrade
-apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
-pip3 install numpy scipy pandas matplotlib ipython
+echo y | apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy scipy pandas matplotlib ipython
