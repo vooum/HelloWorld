fa51f9780631bd2fe57f047f1192a4d1c82f10c8
He Yangle <vooum@qq.com>
2019-11-29 21:43:38 +0800

    annotation

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -6,5 +6,5 @@ sudo apt update
 echo y | sudo apt upgrade
 echo y | sudo apt install gcc g++ gfortran python3{,-dev,-pip,-tk} git cmake vim screen
 pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U # 用清华源升级pip
-pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple # 产生配置文件：~/.config/pip/pip.conf
 echo y | pip3 install numpy scipy pandas matplotlib ipython jupyter
