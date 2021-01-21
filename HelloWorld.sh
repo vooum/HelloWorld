8daa693e505bf79ed014fa0da6a13edaed167c99
vooum <vooum@qq.com>
2021-01-21 18:32:13 +0800

    update

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 默认参数 #
+threads=4 # 编译默认线程数
+ROOT=OFF
+
 # 新机基础安装 #
 if [ $#  -eq 0 ];then
     Upgrade_apt() {
@@ -26,41 +30,43 @@ fi
 
 # 高能程序包 #
 
+while [ $#  -ne 0 ];
+    do
+    case $1 in
+        'ROOT'|'Root'|'root')
+            ROOT=ON
+            ;;
+    esac
+    shift
+done
+
 Install_ROOT() {
     echo y | sudo apt install dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev python2 libssl-dev
     echo y | sudo pip3 install numpy
     echo y | sudo apt install gfortran libpcre3-dev xlibmesa-glu-dev libglew1.5-dev libftgl-dev libmysqlclient-dev libfftw3-dev libcfitsio-dev graphviz-dev libavahi-compat-libdnssd-dev libldap2-dev python-dev libxml2-dev libkrb5-dev libgsl0-dev
 
     curPath=$(readlink -f "$(dirname "$0")")
-    if [ -e ./root_v6.18.04.source.tar.gz ];then
-        echo "root package exist"
-    else
-        wget https://root.cern/download/root_v6.18.04.source.tar.gz
+    if [ ! -e ./root_v6.14.04.source.tar.gz ];then
+        wget https://root.cern/download/root_v6.14.04.source.tar.gz
     fi
-    tar -zxf ./root_v6.18.04.source.tar.gz
+    tar -zxf ./root_v6.14.04.source.tar.gz
     cd /opt/
     sudo mkdir ROOT6
     cd ROOT6
     sudo mkdir builddir installdir
     cd builddir
-    sudo cmake -DCMAKE_INSTALL_PREFIX=../installdir $curPath/root-6.18.04
+    sudo cmake -DCMAKE_INSTALL_PREFIX=../installdir $curPath/root-6.14.04
     sudo sed -i '/minuit2:/s/OFF/ON/g' CMakeCache.txt
     sudo sed -i '/pyroot_legacy:/s/OFF/ON/g' CMakeCache.txt
-    sudo cmake --build . -- install
-    sudo cp ~/.bashrc ~./bashrc.bak.vooum
-    sudo echo 'source /opt/ROOT6/installdir/bin/thisroot.sh' >> ~./bashrc
+    sudo cmake --build . -- install -j$threads
+    sudo cp ~/.bashrc ~/.bashrc.bak.vooum
+    sudo echo 'source /opt/ROOT6/installdir/bin/thisroot.sh # ROOT' >> ~/.bashrc
     ./root-config --has-minuit2
 }
-
-while [ $#  -ne 0 ];
-    do
-    case $1 in
-        'ROOT'|'Root'|'root')
-            Install_ROOT
-            ;;
-    esac
-    shift
-done
+if [ROOT -eq ON]
+then
+    Install_ROOT
+fi
 
 # if [ $#  -eq 0];then
 # exit
